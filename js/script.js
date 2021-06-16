f3e317ccde81e510fe99c370971d33b4a2a6514e
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-06-16 14:23:59 +0200

    Closing project add Snack 2 add comments in JS remove Snack2 from HTML

--- js/script.js
+++ js/script.js
@@ -16,8 +16,10 @@ for(let i = 1; i < parcoBici.length; i++){
     }
 }
 
+//* destrutturo i dati nome e peso da biciLeggera per poterli stampare separatamente
 const {nome,peso} = biciLeggera;
 
+//* tramite "template literal" vado a mostrare nell'HTML i valori prima destrutturati
 document.getElementById('container1').innerHTML = 
 `
 <ul>
@@ -25,3 +27,41 @@ document.getElementById('container1').innerHTML =
     <li>Il peso della bici leggera è = ${peso}kg</li>
 </ul>
 `;
+
+
+
+
+
+
+
+
+
+//TODO Snack2 Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0. Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti. (utilizzate le arrow function). Infine usando la destrutturazione creiamo un nuovo array i cui elementi sono sempre degli oggetti che contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+//* creo il mio array di oggetti
+const squadre = [
+    {nome : 'Inter', puntiFatti : 0, falliSubiti : 0},
+    {nome : 'Milan', puntiFatti : 0, falliSubiti : 0},
+    {nome : 'Juventus', puntiFatti : 0, falliSubiti : 0},
+    {nome : 'Roma', puntiFatti : 0, falliSubiti : 0},
+    {nome : 'Lazio', puntiFatti : 0, falliSubiti : 0},
+    {nome : 'Atalanta', puntiFatti : 0, falliSubiti : 0}
+];
+
+//* creo la funzione che mi restituisce un numero random
+const randomNum = (min,max) => Math.floor(Math.random() * (max - min + 1) ) + min;
+
+//* creo un array vuoto dove andrò ad inserire solo il nome e i falli subiti
+const squadreNew = [];
+
+//* scorro tutti gli elementi dell'array squadre dando dei valori a puntiFatti e falliSubiti. Fatto ciò effettuo il push di nome e falliSubiti dentro al nuovo array
+for(let i = 0; i < squadre.length; i++){
+    squadre[i].puntiFatti = randomNum(1,100);
+    squadre[i].falliSubiti = randomNum(1,20);
+
+    const {nome, falliSubiti} = squadre[i];
+    squadreNew.push({nome,falliSubiti});
+}
+
+//* mostro in console log quanto ricavato dal ciclo
+console.log(squadreNew);
